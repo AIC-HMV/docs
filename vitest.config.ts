317cc5c3ab5aa117eaacdded2ac8ae05662d17e3
Hung Minh Vo (Austin) <austinvo9999@gmail.com>
2025-07-14 07:52:37 -0700

    aichmv_core7_vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,21 +1,23 @@
-# Modify the content of the optimized configuration file
-edited_vitest_config = “””
+aichmv_core7_vitest.config.ts
+
+# Edit the content of the optimized configuration file
+edited_vitest_config = """
 export default {
   test: {
     include: [
-      ‘**/*.test.[jt]s?(x)’,
-      ‘**/*.spec.[jt]s?(x)’,
-      ‘src/**/tests/unit/**/*.[jt]s?(x)’,
-      ‘src/**/tests/integration/**/*.[jt]s?(x)’
+      '**/*.test.[jt]s?(x)',
+      '**/*.spec.[jt]s?(x)',
+      'src/**/tests/unit/**/*.[jt]s?(x)',
+      'src/**/tests/integration/**/*.[jt]s?(x)'
     ],
-    exclude: [‘**/tests/playwright-*.spec.ts’, ‘**/tests/legacy/**’],
+    exclude: ['**/tests/playwright-*.spec.ts', '**/tests/legacy/**'],
     watch: true,
     globals: true,
   },
 }
-“””
+"""
 
-# Replace the file with the modified content
+# Overwrite the file with the updated content
 file_path.write_text(edited_vitest_config)
 
-file_path.name  # Retrieve the filename for verification
+file_path.name  # Return the filename again for confirmation
